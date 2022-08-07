8895280f06ffc75e697aa72776f5c320cbe1ea9c
suidahe <heihesui@gmail.com>
2022-08-08 01:44:36 +0800

    update

---
+++ src/main/java/enumdemo/singleton/LazySingleteon.java
@@ -0,0 +1,40 @@
+package enumdemo.singleton;
+
+public class LazySingleteon {
+    private static LazySingleteon instance = null;
+
+    private LazySingleteon(){
+
+    }
+    /**
+     * 1、适用于单线程环境
+     */
+    public static LazySingleteon getInstanceA(){
+        if(null == instance){
+            instance = new LazySingleteon();
+        }
+        return instance;
+    }
+    /**
+     * 2、适用于多线程环境，但是效率不高
+     */
+    public static synchronized LazySingleteon getInstanceB(){
+        if(instance == null){
+            instance = new LazySingleteon();
+        }
+        return instance;
+    }
+    /**
+     * 3、双重检查加锁
+     */
+    public static LazySingleteon getInstanceC(){
+        if(instance == null){
+            synchronized (LazySingleteon.class){
+                if(instance == null){
+                    instance = new LazySingleteon();
+                }
+            }
+        }
+        return instance;
+    }
+}
