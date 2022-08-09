c3559279ab17ad9b9a052d4d3a01e411df656126
suidahe <heihesui@gmail.com>
2022-08-10 00:29:08 +0800

    update

---
+++ src/main/java/enumdemo/singleton/EnumSingleton.java
@@ -0,0 +1,5 @@
+package enumdemo.singleton;
+
+public enum EnumSingleton {
+    INSTANCE;
+}
---
+++ src/main/java/enumdemo/singleton/HungrySinglon.java
@@ -0,0 +1,31 @@
+package enumdemo.singleton;
+
+public class HungrySinglon {
+    private static final HungrySinglon instance = new HungrySinglon();
+
+    private HungrySinglon() {
+
+    }
+
+    /**
+     * 传统饿汉模式
+     * @return
+     */
+    public static HungrySinglon getInstanceA() {
+        return instance;
+    }
+
+    /**
+     * 静态内部类实现饿汉模式
+     */
+    private static class HungerSingletonHolder{
+        private static final HungrySinglon instance = new HungrySinglon();
+    }
+    public static HungrySinglon getInstanceB(){
+        return HungerSingletonHolder.instance;
+    }
+
+    /**
+     *
+     */
+}
