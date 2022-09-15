74fdd3b168774eddf508a2077420c099dd35ef70
charlessui <charles.sui@grapecity.com>
2022-09-16 00:54:58 +0800

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,5 @@
-import upgrade.sealed.Circle;
-
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle(1);
-        Circle circle1 = new Circle(1);
-        System.out.println(circle1 == circle);
-        System.out.println(circle1.equals(circle));
+        String stringBlock;
     }
 }
--- src/main/java/upgrade/vector/LinearEquation.java
+++ src/main/java/upgrade/vector/LinearEquation.java
@@ -1,69 +1,28 @@
 package upgrade.vector;
 
 import jdk.incubator.vector.FloatVector;
-import upgrade.nullp.Returned;
-import upgrade.nullp.Returned.ErrorCode;
-import upgrade.nullp.Returned.ReturnValue;
-
-import static upgrade.nullp.Returned.Undefined;
 
 public class LinearEquation {
-    private static final float[] a = new float[]{0.6F, 0.7F, 0.8F, 0.9F};
-    static final FloatVector va =
-            FloatVector.fromArray(FloatVector.SPECIES_128, a, 0);
-    private static final float[] x = new float[]{1.0F, 2.0F, 3.0F, 4.0F};
-    static final FloatVector vx =
-            FloatVector.fromArray(FloatVector.SPECIES_128, x, 0);
-
-    private static Returned<Float> sumInScalar(float[] a, float[] x) {
-        if (a == null || x == null || a.length != x.length) {
-            return new ErrorCode(-1);
-        }
-
-        float[] y = new float[a.length];
-        for (int i = 0; i < a.length; i++) {
-            y[i] = a[i] * x[i];
-        }
-
-        float r = 0F;
-        for (int i = 0; i < y.length; i++) {
-            r += y[i];
-        }
-
-        return new ReturnValue<>(r);
-    }
-
-    private static Returned<Float> sumInVector(FloatVector va, FloatVector vx) {
-        if (va == null || vx == null || va.length() != vx.length()) {
-            return new ErrorCode(-1);
-        }
-
-        // FloatVector vy = va.mul(vx);
-        float[] y = va.mul(vx).toArray();
-
-        float r = 0F;
-        for (int i = 0; i < y.length; i++) {
-            r += y[i];
-        }
-        return new Returned.ReturnValue<>(r);
-    }
+    private static final float[] self = new float[]{5, 3, 3, 0};
+    static final FloatVector selfVector =
+            FloatVector.fromArray(FloatVector.SPECIES_PREFERRED, self, 0);
+    private static final float[] xiaoming = new float[]{4, 5, 2, 1};
+    static final FloatVector xiaomingVector =
+            FloatVector.fromArray(FloatVector.SPECIES_PREFERRED, xiaoming, 0);
 
     public static void main(String[] args) {
-        Returned<Float> rt = LinearEquation.sumInScalar(a, x);
-        switch (rt) {
-            case ReturnValue rv -> {
-                // Get the returned value
-                if (rv.returnValue() instanceof Float result) {
-                    System.out.println("The summary is: " + result);
-                } else {  // unlikely
-                    System.out.println(
-                            "Implementation error: LinearEquation.sumInScalar");
-                }
-            }
-            case ErrorCode ec -> System.out.println("Invalid input parameters");
-            case Undefined undefined -> {
-                System.out.println("return null result");
-            }
+        double result = 0;
+        for (int i = 0; i < self.length; i++) {
+            result = Math.pow(self[i] - xiaoming[i], 2);
         }
+        result = Math.sqrt(result);
+        double resultVector = 0;
+        float[] floats = selfVector.sub(xiaomingVector).pow(2).toArray();
+        for (int i = 0; i < floats.length; i++) {
+            resultVector += floats[i];
+        }
+        resultVector = Math.sqrt(resultVector);
     }
+
+
 }
