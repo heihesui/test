5aa57376c0cd9e411e110a7887d1c16a4cf61434
charlessui <charles.sui@grapecity.com>
2022-09-16 08:49:30 +0800

    update

--- src/main/java/upgrade/vector/LinearEquation.java
+++ src/main/java/upgrade/vector/LinearEquation.java
@@ -5,15 +5,15 @@ import jdk.incubator.vector.FloatVector;
 public class LinearEquation {
     private static final float[] self = new float[]{5, 3, 3, 0};
     static final FloatVector selfVector =
-            FloatVector.fromArray(FloatVector.SPECIES_PREFERRED, self, 0);
+            FloatVector.fromArray(FloatVector.SPECIES_128, self, 0);
     private static final float[] xiaoming = new float[]{4, 5, 2, 1};
     static final FloatVector xiaomingVector =
-            FloatVector.fromArray(FloatVector.SPECIES_PREFERRED, xiaoming, 0);
+            FloatVector.fromArray(FloatVector.SPECIES_128, xiaoming, 0);
 
     public static void main(String[] args) {
         double result = 0;
         for (int i = 0; i < self.length; i++) {
-            result = Math.pow(self[i] - xiaoming[i], 2);
+            result += Math.pow(self[i] - xiaoming[i], 2);
         }
         result = Math.sqrt(result);
         double resultVector = 0;
